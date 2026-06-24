ba4dc44c17032401982e9f068c9ecd6423213ca1
hamzalchgr <hamzalchgrdev@gmail.com>
2026-06-24 23:55:21 +0200

    Auth page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Layout from './components/layout/Layout';
 import Shop from './pages/store/Shop';
+import Auth from './pages/authentication/Auth';
 
 const App = () => {
    return (
@@ -11,6 +12,7 @@ const App = () => {
             <Route index path="/" element={<Home />} />
             <Route path="/shop/:categ" element={<Shop />} />
          </Route>
+            <Route path="/account" element={<Auth />} />
       </Routes>
    );
 };
